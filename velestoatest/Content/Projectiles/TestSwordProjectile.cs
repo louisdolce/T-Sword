eaed9eae3a380f5c70b0362cea56091643b428d1
louisdolce <163790895+louisdolce@users.noreply.github.com>
2024-08-22 23:56:59 -0400

    Add files via upload
    Base Upload

---
+++ velestoatest/Content/Projectiles/TestSwordProjectile.cs
@@ -0,0 +1,9 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader
+
+namespace velestoatest.Content.Projectiles
+{
+public class TestSwordProjectile : ModProjectile    
+}
